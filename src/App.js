312426a916ccde007d6dea6ab8ac4148ce3184dc
Andreas <mchoffn@hotmail.com>
2023-12-13 01:28:41 +0100

    indentation app.js

--- src/App.js
+++ src/App.js
@@ -8,20 +8,19 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 
 const App = () => {
-  return (
-    <Router>
-        <Navbar />
-        <div className="main-content">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/admin-dashboard" element={<AdminDashboard />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </div>
-      
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <div className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/admin-dashboard" element={<AdminDashboard />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
