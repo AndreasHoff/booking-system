a86c40e0175c3d73f1db9f4a334a6095470569cd
Andreas <mchoffn@hotmail.com>
2023-12-26 20:24:56 +0100

    introducing tailwind

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -4,55 +4,51 @@ import { db } from '../firebase';
 import '../styles/settings.css';
 
 const Settings = () => {
-  const [category, setCategory] = useState('');
-  const [serviceName, setServiceName] = useState('');
-  const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [serviceName, setServiceName] = useState('');
+    const [description, setDescription] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    try {
-      // Add or update the category document
-      const categoryDocRef = doc(db, 'categories', category);
-      await setDoc(categoryDocRef, {});
+        try {
+        // Add or update the category document
+        const categoryDocRef = doc(db, 'categories', category);
+        await setDoc(categoryDocRef, {});
 
-      // Add a new service to the category
-      const serviceRef = collection(categoryDocRef, 'services');
-      await addDoc(serviceRef, { name: serviceName, description });
+        // Add a new service to the category
+        const serviceRef = collection(categoryDocRef, 'services');
+        await addDoc(serviceRef, { name: serviceName, description });
 
-      // Clear form fields after successful submission
-      setCategory('');
-      setServiceName('');
-      setDescription('');
+        // Clear form fields after successful submission
+        setCategory('');
+        setServiceName('');
+        setDescription('');
 
-      console.log('Data successfully written to Firestore!');
-    } catch (error) {
-      console.error('Error writing data to Firestore:', error);
-    }
-  };
+        console.log('Data successfully written to Firestore!');
+        } catch (error) {
+        console.error('Error writing data to Firestore:', error);
+        }
+    };
 
-  return (
+return (
     <div className="settings-form">
-      <h2>Add or edit a Service</h2>
-      <form onSubmit={handleSubmit}>
-
-        <label>
-          Category
-          <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
-        </label>
-
-        <label>
-          Service Name:
-          <input type="text" value={serviceName} onChange={(e) => setServiceName(e.target.value)} />
-        </label>
-
-        <label>
-          Description:
-          <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
-        </label>
-
-        <button type="submit">Submit</button>
-      </form>
+        <h2>Add or edit a Service</h2>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Category
+                <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
+            </label>
+            <label>
+                Service Name:
+                <input type="text" value={serviceName} onChange={(e) => setServiceName(e.target.value)} />
+            </label>
+            <label>
+                Description:
+                <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
     </div>
   );
 };
---
+++ src/tailwind.config.ts
@@ -0,0 +1,13 @@
+// tailwind.config.js or tailwind.config.ts
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  // other configurations...
+};
