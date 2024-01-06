761de88c76d6d2ec0a5a05db305311d14ecac7f2
Andreas <mchoffn@hotmail.com>
2024-01-06 01:21:39 +0100

    added prime react toaster for success booking and modified translation file for text keys

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,5 +1,6 @@
 import { addDoc, collection, getDocs, onSnapshot, serverTimestamp } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
+import { Toast } from 'primereact/toast';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { db } from '../firebase';
 import '../styles/Booking.css';
@@ -16,7 +17,8 @@ const Booking = () => {
     const [comment, setComment] = useState('');
     const [termsAccepted, setTermsAccepted] = useState(false);
     const [isAccordionOpen, setAccordionOpen] = useState(false);
-    const { t, i18n } = useTranslation();
+    const { t: translate, i18n } = useTranslation();
+    const toast = useRef(null);
 
     useEffect(() => {
         const unsubscribe = onSnapshot(collection(db, 'categories'), async (snapshot) => {
@@ -29,7 +31,7 @@ const Booking = () => {
             setCategories(categoriesData);
         });
 
-        i18n.changeLanguage('da'); // Change language to Danish
+        i18n.changeLanguage('da');
 
         return () => unsubscribe();
     }, [i18n]);
@@ -40,15 +42,27 @@ const Booking = () => {
         setAccordionOpen(!isAccordionOpen); // Toggle accordion open/close
     };
 
+    const resetForm = () => {
+        setSelectedCategory('');
+        setSelectedService('');
+        setSelectedServiceId('');
+        setSelectedDate('');
+        setFullName('');
+        setEmail('');
+        setPhoneNumber('');
+        setComment('');
+        setTermsAccepted(false);
+        setAccordionOpen(false);
+    };
+
     const handleConfirm = async () => {
         if (!termsAccepted) {
-            console.log(t('booking.accept_terms.alert'));
-            alert(t('booking.accept_terms.alert'));
+            alert(translate('booking.accept_terms.alert'));
             return;
         }
     
         if (!selectedCategory) {
-            alert('Please select a category');
+            alert(translate('booking.select_category.alert'));
             return;
         }
     
@@ -57,7 +71,7 @@ const Booking = () => {
             return;
         }
     
-        const docRef = await addDoc(collection(db, 'bookings'), {
+        await addDoc(collection(db, 'bookings'), {
             category: selectedCategory,
             service: selectedService,
             date: selectedDate,
@@ -81,6 +95,10 @@ const Booking = () => {
         
         writeToFirebase(data);
 
+        toast.current.show({severity:'success', summary: 'Successful Booking', detail:'Your booking has been confirmed', life: 3000});
+
+        resetForm();
+
         function writeToFirebase(data) {
             console.log("Data being written to Firebase: ", data);
         }
@@ -89,87 +107,87 @@ const Booking = () => {
 
     return (
         <div className="booking-container">
-    <div>
-    <div>
-        <label>Select a category</label>
-        <select
-            value={selectedCategory}
-            onChange={(e) => handleCategoryChange(e.target.value)}
-        >
-            <option value="">
-                Choose a category
-            </option>
-            {categories.map((category) => (
-                <option key={category.id} value={category.id}>
-                    {category.id}
+            <Toast ref={toast} />
+            <div>
+                <div>
+                    <label>Select a category</label>
+                    <select
+                        value={selectedCategory}
+                        onChange={(e) => handleCategoryChange(e.target.value)}
+                    >
+                        <option value="">
+                            Choose a category
+                        </option>
+                        {categories.map((category) => (
+                        <option key={category.id} value={category.id}>
+                            {category.id}
+                        </option>
+                        ))}
+                    </select>
+                </div>
+            {selectedCategory && (
+            <div>
+            <label>Select a service</label>
+            <select 
+                value={selectedServiceId} 
+                onChange={(e) => {
+                    const selectedServiceObject = categories
+                        .find((category) => category.id === selectedCategory)
+                        .services
+                        .find((service) => service.id === e.target.value);
+
+                    if (selectedServiceObject) {
+                        setSelectedService(selectedServiceObject.name);
+                        setSelectedServiceId(selectedServiceObject.id);
+                    } else {
+                        setSelectedService(null);
+                        setSelectedServiceId(null);
+                    }
+                }}
+            >
+                <option value="">
+                    Choose a service
                 </option>
-            ))}
-        </select>
-    </div>
-
-    {selectedCategory && (
-    <div>
-        <label>Select a service</label>
-        <select 
-            value={selectedServiceId} 
-            onChange={(e) => {
-                const selectedServiceObject = categories
-                    .find((category) => category.id === selectedCategory)
-                    .services
-                    .find((service) => service.id === e.target.value);
-
-                if (selectedServiceObject) {
-                    setSelectedService(selectedServiceObject.name);
-                    setSelectedServiceId(selectedServiceObject.id);
-                } else {
-                    setSelectedService(null);
-                    setSelectedServiceId(null);
-                }
-            }}
-        >
-            <option value="">
-                Choose a service
-            </option>
-            {categories
-                .find((category) => category.id === selectedCategory)
-                .services
-                .map((service) => (
-                    <option key={service.id} value={service.id}>
-                        {service.name}
-                    </option>
-                ))
-            }
-        </select>
-        {selectedService && (
-            <p>
                 {categories
                     .find((category) => category.id === selectedCategory)
                     .services
-                    .find((service) => service.name === selectedService)
-                    .description}
-            </p>
-        )}
-    </div>
-)}
+                    .map((service) => (
+                        <option key={service.id} value={service.id}>
+                            {service.name}
+                        </option>
+                    ))
+                }
+            </select>
+            {selectedService && (
+                <p>
+                    {categories
+                        .find((category) => category.id === selectedCategory)
+                        .services
+                        .find((service) => service.name === selectedService)
+                        .description}
+                </p>
+            )}
+        </div>
+    )}
 
-        <input type="date" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
-        <h2>Personal Information</h2>
-        <form onSubmit={(e) => e.preventDefault()} className="booking-form">
-            <input type="text" placeholder="Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)} />
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="tel" placeholder="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-            <textarea placeholder="Comment" value={comment} onChange={(e) => setComment(e.target.value)} />
-            <label>
-                <input type="checkbox" checked={termsAccepted} onChange={(e) => setTermsAccepted(e.target.checked)} />
-                I accept the terms
-            </label>
+            <input type="date" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
+            <h2>Personal Information</h2>
+            <form onSubmit={(e) => e.preventDefault()} className="booking-form">
+                <input type="text" placeholder="Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)} />
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="tel" placeholder="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                <textarea placeholder="Comment" value={comment} onChange={(e) => setComment(e.target.value)} />
+                <label>
+                    <input type="checkbox" checked={termsAccepted} onChange={(e) => setTermsAccepted(e.target.checked)} />
+                    I accept the terms
+                </label>
 
-            <button type="button" onClick={handleConfirm}>
-                Confirm
-            </button>
-        </form>
+                <button type="button" onClick={handleConfirm}>
+                    Confirm
+                </button>
+            </form>
+        </div>
     </div>
-</div>
     );
 };
 
